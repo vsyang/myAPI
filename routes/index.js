b38674e638db4144e8a7abb6a51c988844d1bc68
vsyang <yang.vanessa85@gmail.com>
2025-11-15 14:25:42 -0700

    added links to index

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,14 @@ router.use('/', require('./swagger'));
 // Example default route
 router.get('/', (req, res) => {
   //#swagger.tags=['Hello World']
-  res.send('Hello World!');
+  res.send(`
+    <h1>Welcome to My API</h1>
+    <p>Click a link below to view the different routes:</p>
+    <ul>
+      <li><a href="/owners">Pet Owners</a></li>
+      <li><a href="/pets">Pets</a></li>
+      <li><a href="/api-docs">Swagger Documentation</a></li>
+  `);
 });
 
 // Main resource routes
