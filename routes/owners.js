a4abd4033fbf5b2674332759cac690ec2ffadfe7
vsyang <yang.vanessa85@gmail.com>
2025-11-20 19:51:35 -0700

    readded validation to pet and owners js

--- routes/owners.js
+++ routes/owners.js
@@ -4,18 +4,12 @@ const router = express.Router();
 const ownersController = require('../controllers/owners');
 
 const { isAuthenticated } = require('../middleware/authenticate');
+const validation = require('../middleware/validate');
 
 router.get('/', ownersController.getAllOwners);
 router.get('/:id', ownersController.getSingleOwner);
-router.post('/', isAuthenticated, ownersController.createOwner);
-router.put('/:id', isAuthenticated, ownersController.updateOwner);
+router.post('/', isAuthenticated, validation.saveOwner, ownersController.createOwner);
+router.put('/:id', isAuthenticated, validation.saveOwner, ownersController.updateOwner);
 router.delete('/:id', isAuthenticated, ownersController.deleteOwner);
 
 module.exports = router;
-
-//***** These were for when validation was needed *****/
-// const validation = require('../middleware/validate');
-
-// router.post('/', validation.saveOwner, ownersController.createOwner);
-// router.put('/:id', validation.saveOwner, ownersController.updateOwner);
-// router.delete('/:id', ownersController.deleteOwner);
--- routes/pets.js
+++ routes/pets.js
@@ -4,18 +4,12 @@ const router = express.Router();
 const petsController = require('../controllers/pets');
 
 const { isAuthenticated } = require('../middleware/authenticate');
+const validation = require('../middleware/validate');
 
 router.get('/', petsController.getAllPets);
 router.get('/:id', petsController.getSinglePet);
-router.post('/', isAuthenticated, petsController.createPet);
-router.put('/:id', isAuthenticated, petsController.updatePet);
+router.post('/', isAuthenticated, validation.savePet, petsController.createPet);
+router.put('/:id', isAuthenticated, validation.savePet, petsController.updatePet);
 router.delete('/:id',isAuthenticated, petsController.deletePet);
 
 module.exports = router;
-
-//***** These were for when validation was needed *****/
-// const validation = require('../middleware/validate');
-
-// router.post('/', validation.savePet, petsController.createPet);
-// router.put('/:id', validation.savePet, petsController.updatePet);
-// router.delete('/:id', petsController.deletePet);
